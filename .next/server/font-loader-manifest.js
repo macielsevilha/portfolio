f9246db1f7ead59932c83afd06431ae7f06c13a0
Maciel <dev.macielsevilha@example.com>
2023-10-16 19:16:10 -0300

    add fundo

--- .next/server/font-loader-manifest.js
+++ .next/server/font-loader-manifest.js
@@ -1,10 +1,1 @@
-self.__FONT_LOADER_MANIFEST={
-  "pages": {},
-  "app": {
-    "C:\\github\\portfolio\\node_modules\\next\\font\\google\\target.css?{\"path\":\"src\\\\app\\\\page.jsx\",\"import\":\"Inter\",\"arguments\":[{\"subsets\":[\"latin\"]}],\"variableName\":\"inter\"}": [
-      "static/media/c9a5bc6a7c948fb0-s.p.woff2"
-    ]
-  },
-  "appUsingSizeAdjust": true,
-  "pagesUsingSizeAdjust": false
-}
+self.__FONT_LOADER_MANIFEST={pages:{},app:{'C:\\github\\portfolio\\node_modules\\next\\font\\google\\target.css?{"path":"src\\\\app\\\\page.jsx","import":"Inter","arguments":[{"subsets":["latin"]}],"variableName":"inter"}':["static/media/c9a5bc6a7c948fb0-s.p.woff2"]},appUsingSizeAdjust:!0,pagesUsingSizeAdjust:!1};
