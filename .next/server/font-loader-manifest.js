3cd95a279e6469b7e141afeefe536ed59a1c678f
Maciel <dev.macielsevilha@example.com>
2023-11-13 11:47:44 -0300

    recomecando projeto

--- .next/server/font-loader-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__FONT_LOADER_MANIFEST={pages:{},app:{'C:\\github\\portfolio\\node_modules\\next\\font\\google\\target.css?{"path":"src\\\\app\\\\page.jsx","import":"Inter","arguments":[{"subsets":["latin"]}],"variableName":"inter"}':["static/media/c9a5bc6a7c948fb0-s.p.woff2"]},appUsingSizeAdjust:!0,pagesUsingSizeAdjust:!1};
--- src/app/components/surfaces/Footer.jsx
+++
@@ -1,7 +0,0 @@
-import "../../sass/app.scss";
-
-export function Footer() {
-    return (
-        <h1 className="portfolio-title--xl p-10 text-center">Rodapé</h1>
-    )
-}
